b202560d87a65d21cb370abce009bd6b0130b53f
Mapc1 <marcocosta579@gmail.com>
2022-08-14 19:27:19 +0100

    Start Project

---
+++ Source/AlienRacesMod.cs
@@ -0,0 +1,12 @@
+using Verse;
+
+namespace AlienRaces;
+
+[StaticConstructorOnStartup]
+public static class AlienRacesMod
+{
+    static AlienRacesMod()
+    {
+        Log.Message("Alien Races Mod loaded successfully!");
+    }
+}
